1fd475249f6eced1d3229c7b8732d22dd07bd3cb
Marcela Rendon <marcis23@gmail.com>
2024-12-02 17:50:21 -0500

    agrego componentes Form y Navbar

--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -1,13 +1,72 @@
-import React from "react";
+import React, { useState, useContext } from "react";
+import { ContextGlobal } from "../Components/utils/global.context";
 
 
 const Form = () => {
-  //Aqui deberan implementar el form completo con sus validaciones
+  const { state } = useContext(ContextGlobal); 
+  const [formData, setFormData] = useState ({
+  nombre: "",
+  email: "",
+});
+const [error, setError] = useState ("");
+const [successMessage, setSuccessMessage] = useState ("");
+
+const handleChange = (e) => {
+  const {name, value} = e.target;
+  setFormData ({...formData, [name]: value});
+};
+
+const handleSubmit = (e) => {
+  e.preventDefault ();
+  setError ("");
+  setSuccessMessage ("");
+
+  if (formData.nombre.length <= 5) {
+    setError ("El nombre completo debe tener más de 5 caracteres");
+  return;
+}
+
+
+const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+if(!emailRegex.test (formData.email)) {
+  setError("Por favor ingrese un email válido.");
+  return;
+}
+console.log("Datos enviados:", formData);
+    setSuccessMessage(`Gracias ${formData.nombre}, te contactaremos cuando antes vía mail.`);
+    setFormData({ nombre: "", email: "" }); 
+  };
 
   return (
-    <div>
-      <form>
-      </form>
+    <div className={`form-container ${state.theme}`}>
+    <form onSubmit={handleSubmit} className = {`contact-form ${state.theme}`}>
+    <div className="form-group">
+    <label htmlFor="nombre">Nombre Completo:</label>
+    <input 
+    type= "text"
+    id= "nombre"
+    name= "nombre"
+    value= {formData.nombre}
+    onChange= {handleChange}
+    placeholder="Escribe tu nombre completo"
+    />
+    </div>
+    <div className="form-group">
+    <label htmlFor="email">Email:</label>
+    <input
+          type="email"
+          id="email"
+          name="email"
+          value={formData.email}
+          onChange={handleChange}
+          placeholder="Escribe tu email"
+        />
+    </div>
+
+    <button type="submit">Enviar</button>
+    {error && <p className="error-message">{error}</p>}
+    {successMessage && <p className="success-message">{successMessage}</p>}
+    </form>
     </div>
   );
 };
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,16 +1,30 @@
-import React from 'react'
-
-//Este componente debera ser estilado como "dark" o "light" dependiendo del theme del Context
+import React, { useContext } from "react";
+import { Link } from "react-router-dom";
+import { ContextGlobal } from "../Components/utils/global.context";
 
 const Navbar = () => {
+  const { state, dispatch } = useContext(ContextGlobal);
+  const handleTheme = () => {
+    dispatch({ type: "HANDLE_THEME" });
+  };
 
   return (
-    <nav>
-      {/* Aqui deberan agregar los liks correspondientes a las rutas definidas */}
-      {/* Deberan implementar ademas la logica para cambiar de Theme con el button */}
-      <button>Change theme</button>
-    </nav>
-  )
-}
+    <nav className={`navbar ${state.theme}`}>
+      <div className="nav-logo">
+        <img src="/images/DH-odonto.png" alt="Logo" className="logo" />
+      </div>
 
-export default Navbar
+      <div className="nav-links">
+        <Link to="/">Home</Link>
+        <Link to="/contacto">Contacto</Link>
+        <Link to="/favoritos">Favoritos</Link>
+      </div>
+      
+      <button onClick={handleTheme} className="handleThemeButton">
+      {state.theme === "light" ? "🌙" : "☀️"} 
+      </button>
+    </nav>
+  );
+};
+
+export default Navbar;
