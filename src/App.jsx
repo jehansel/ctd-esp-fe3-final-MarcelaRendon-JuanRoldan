ea549e883713f371c227b5dbf578cdb0a7b13eac
Marcela Rendon <marcis23@gmail.com>
2024-12-02 17:32:04 -0500

    generando rutas en App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,24 @@
-
+import React, { useContext } from "react";
+import { ContextGlobal } from "./Components/utils/global.context";
 import Footer from "./Components/Footer";
 import Navbar from "./Components/Navbar";
+import Home from "./Routes/Home";
+import Contact from "./Routes/Contact";
+import Detail from "./Routes/Detail"
+import Favs from "./Routes/Favs"
+import {Routes, Route } from "react-router-dom";
 
 
 function App() {
   return (
       <div className="App">
           <Navbar/>
+          <Routes>
+            <Route path= "/" element={<Home />} />
+            <Route path= "/Contacto" element={<Contact />} />
+            <Route path= "/dentist/:id" element={<Detail />} />
+            <Route path= "/favoritos" element={<Favs />} />
+            </Routes>
           <Footer/>
       </div>
   );
